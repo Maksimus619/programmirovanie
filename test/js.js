78656a10e1def41f0f75bc213654b91b0f341afb
Maksimus619 <124022097+Maksimus619@users.noreply.github.com>
2023-12-07 21:21:15 +0300

    Add files via upload

--- test/js.js
+++ test/js.js
@@ -24,3 +24,32 @@ const slide = () => {
 window.addEventListener('load', () => {
   slide();
 });
+var modals = document.querySelectorAll(".modal");
+var triggers = document.querySelectorAll(".trigger");
+var closeButtons = document.querySelectorAll(".close-button");
+
+function toggleModal() {
+    var modal = this.nextElementSibling;
+    modal.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+    modals.forEach(function (modal) {
+        if (event.target === modal) {
+            modal.classList.remove("show-modal");
+        }
+    });
+}
+
+triggers.forEach(function (trigger) {
+    trigger.addEventListener("click", toggleModal);
+});
+
+closeButtons.forEach(function (closeButton) {
+    closeButton.addEventListener("click", function () {
+        var modal = this.closest(".modal");
+        modal.classList.remove("show-modal");
+    });
+});
+
+window.addEventListener("click", windowOnClick);
